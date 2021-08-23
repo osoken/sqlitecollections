5daaf74a88a108b7362133a230df64758627a0e5
osoken <osoken.devel@outlook.jp>
2021-08-24 01:52:17 +0900

    implement rebuild option

--- sqlitecollections/core.py
+++ sqlitecollections/core.py
@@ -2,14 +2,14 @@ import sqlite3
 import sys
 from abc import ABCMeta, abstractmethod
 from collections.abc import Hashable
+from enum import Enum
 from pickle import dumps, loads
 from tempfile import NamedTemporaryFile
 from typing import Callable, Generic, Optional, Tuple, TypeVar, Union, cast
 from uuid import uuid4
 
 if sys.version_info >= (3, 9):
-    from collections.abc import (Iterable, Iterator, Mapping, MutableMapping,
-                                 Reversible)
+    from collections.abc import Iterable, Iterator, Mapping, MutableMapping, Reversible
 else:
     from typing import Iterable, Iterator, Mapping, MutableMapping
 if sys.version_info >= (3, 8):
@@ -21,6 +21,12 @@ KT = TypeVar("KT")
 VT = TypeVar("VT")
 
 
+class RebuildStrategy(Enum):
+    CHECK_WITH_FIRST_ELEMENT = 1
+    ALWAYS = 2
+    SKIP = 3
+
+
 class SqliteCollectionBase(Generic[T], metaclass=ABCMeta):
     def __init__(
         self,
@@ -29,11 +35,14 @@ class SqliteCollectionBase(Generic[T], metaclass=ABCMeta):
         serializer: Optional[Callable[[T], bytes]] = None,
         deserializer: Optional[Callable[[bytes], T]] = None,
         destruct_table_on_delete: bool = False,
+        rebuild_strategy: RebuildStrategy = RebuildStrategy.CHECK_WITH_FIRST_ELEMENT,
+        do_initialize: bool = True,
     ):
         super(SqliteCollectionBase, self).__init__()
         self._serializer = cast(Callable[[T], bytes], dumps) if serializer is None else serializer
         self._deserializer = cast(Callable[[bytes], T], loads) if deserializer is None else deserializer
         self._destruct_table_on_delete = destruct_table_on_delete
+        self._rebuild_strategy = rebuild_strategy
         if connection is None:
             self._connection = sqlite3.connect(NamedTemporaryFile().name)
         elif isinstance(connection, str):
@@ -47,7 +56,8 @@ class SqliteCollectionBase(Generic[T], metaclass=ABCMeta):
         self._table_name = (
             f"{self.container_type_name}_{str(uuid4()).replace('-', '')}" if table_name is None else table_name
         )
-        self._initialize(commit=True)
+        if do_initialize:
+            self._initialize(commit=True)
 
     def __del__(self) -> None:
         if self.destruct_table_on_delete:
@@ -62,6 +72,27 @@ class SqliteCollectionBase(Generic[T], metaclass=ABCMeta):
     def _initialize(self, commit: bool = False) -> None:
         self._initialize_metadata_table(commit=commit)
         self._initialize_table(commit=commit)
+        if self._should_rebuild():
+            self._do_rebuild(commit=commit)
+
+    def _should_rebuild(self) -> bool:
+        if self.rebuild_strategy == RebuildStrategy.ALWAYS:
+            return True
+        if self.rebuild_strategy == RebuildStrategy.SKIP:
+            return False
+        return self._rebuild_check_with_first_element()
+
+    @abstractmethod
+    def _rebuild_check_with_first_element(self) -> bool:
+        ...
+
+    @abstractmethod
+    def _do_rebuild(self, commit: bool = False) -> None:
+        ...
+
+    @property
+    def rebuild_strategy(self) -> RebuildStrategy:
+        return self._rebuild_strategy
 
     @property
     def destruct_table_on_delete(self) -> bool:
@@ -176,6 +207,7 @@ class _Dict(Generic[KT, VT], SqliteCollectionBase[VT], MutableMapping[KT, VT]):
         key_serializer: Optional[Callable[[KT], bytes]] = None,
         key_deserializer: Optional[Callable[[bytes], KT]] = None,
         destruct_table_on_delete: bool = False,
+        rebuild_strategy: RebuildStrategy = RebuildStrategy.CHECK_WITH_FIRST_ELEMENT,
         data: Optional[Mapping[KT, VT]] = None,
         **kwargs: VT,
     ) -> None:
@@ -185,15 +217,16 @@ class _Dict(Generic[KT, VT], SqliteCollectionBase[VT], MutableMapping[KT, VT]):
             serializer=serializer,
             deserializer=deserializer,
             destruct_table_on_delete=destruct_table_on_delete,
+            rebuild_strategy=rebuild_strategy,
+            do_initialize=False,
         )
         self._key_serializer = (
-            cast(Callable[[KT], bytes], self._serializer) if key_serializer is None else key_serializer
+            cast(Callable[[KT], bytes], self.serializer) if key_serializer is None else key_serializer
         )
         self._key_deserializer = (
-            cast(Callable[[bytes], KT], self._deserializer) if key_deserializer is None else key_deserializer
+            cast(Callable[[bytes], KT], self.deserializer) if key_deserializer is None else key_deserializer
         )
-        if self._should_rebuild():
-            self._do_rebuild(commit=True)
+        self._initialize(commit=True)
         self.update({} if data is None else data, **kwargs)
 
     @property
@@ -219,7 +252,7 @@ class _Dict(Generic[KT, VT], SqliteCollectionBase[VT], MutableMapping[KT, VT]):
         if commit:
             self._connection.commit()
 
-    def _should_rebuild(self) -> bool:
+    def _rebuild_check_with_first_element(self) -> bool:
         cur = self.connection.cursor()
         cur.execute(f"SELECT serialized_key FROM {self.table_name} ORDER BY item_order LIMIT 1")
         res = cur.fetchone()
