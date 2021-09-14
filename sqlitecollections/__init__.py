e51f4654740c3fe95d65e0d5712be0e86c1a4cf4
osoken <osoken.devel@outlook.jp>
2021-09-14 20:36:30 +0900

    update minor version

--- sqlitecollections/__init__.py
+++ sqlitecollections/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.5.0"
+__version__ = "0.5.2"
 __author__ = "osoken"
 __description__ = "Python collections that are backended by sqlite3 DB and are compatible with the built-in collections"
 __long_description__ = __description__
