2e4b34d56c07ed5341738b48a4b775fa36cc6d0f
osoken <osoken@users.noreply.github.com>
2021-10-09 14:30:54 +0000

    build: renew version 0.5.6

--- sqlitecollections/__init__.py
+++ sqlitecollections/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.5.5"
+__version__ = "0.5.6"
 __author__ = "osoken"
 __description__ = "Python collections that are backended by sqlite3 DB and are compatible with the built-in collections"
 __email__ = "osoken.devel@outlook.jp"
