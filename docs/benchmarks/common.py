a7c7e7531079320134fc831840056839fe678ccf
osoken <osoken.devel@outlook.jp>
2021-11-12 23:51:47 +0900

    feature(docs/benchmarks): add BenchmarkResult class

--- docs/benchmarks/common.py
+++ docs/benchmarks/common.py
@@ -1,18 +1,37 @@
-import gc
 import statistics
+import sys
 import time
 from abc import ABCMeta, abstractmethod
 from timeit import timeit
 from typing import Any, Tuple
 
+if sys.version_info > (3, 9):
+    from collections.abc import Dict
+else:
+    from typing import Dict
+
 from memory_profiler import memory_usage, profile
+
+
+class BenchmarkResult:
+    def __init__(self, timing: float, memory: float):
+        self._timing = timing
+        self._memory = memory
+
+    @property
+    def timing(self) -> float:
+        return self._timing
+
+    @property
+    def memory(self) -> float:
+        return self._memory
+
+    def dict(self) -> Dict[str, float]:
+        return {"timing": self.timing, "memory": self.memory}
 
 
 class BenchmarkBase(metaclass=ABCMeta):
     def __init__(self, number: int = 32, interval: float = 0.01):
-        self._memory_after_setup_buffer = []
-        self._memory_during_exec_buffer = []
-        self._timing_buffer = []
         self._number = number
         self._interval = interval
 
@@ -35,10 +54,11 @@ class BenchmarkBase(metaclass=ABCMeta):
     def _get_current_memory(self) -> float:
         return memory_usage((lambda: None,), max_usage=True)
 
-    def __call__(self) -> None:
+    def __call__(self) -> BenchmarkResult:
+        memory_after_setup_buffer = []
+        memory_during_exec_buffer = []
+        timing_buffer = []
         for i in range(self._number):
-            gc.collect()
-            gc.collect()
             self.setup()
             memory_after_setup = self._get_current_memory()
 
@@ -51,11 +71,10 @@ class BenchmarkBase(metaclass=ABCMeta):
                 raise AssertionError()
             self.teardown()
             timing = t2 - t1
-            self._timing_buffer.append(timing)
-            self._memory_after_setup_buffer.append(memory_after_setup)
-            self._memory_during_exec_buffer.append(memory_during_exec)
-
-    def summary(self) -> Tuple[float, float]:
-        return statistics.mean(self._timing_buffer), max(
-            m2 - m1 for m1, m2 in zip(self._memory_after_setup_buffer, self._memory_during_exec_buffer)
+            timing_buffer.append(timing)
+            memory_after_setup_buffer.append(memory_after_setup)
+            memory_during_exec_buffer.append(memory_during_exec)
+        return BenchmarkResult(
+            statistics.mean(timing_buffer),
+            max(m2 - m1 for m1, m2 in zip(memory_after_setup_buffer, memory_during_exec_buffer)),
         )
