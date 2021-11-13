6bb2b1daa7ee3f1e99f33a914771ca9f9510b141
osoken <osoken.devel@outlook.jp>
2021-11-13 23:16:04 +0900

    feature(docs/benchmarks): add Comparison and ComparisonResult class

--- docs/benchmarks/common.py
+++ docs/benchmarks/common.py
@@ -6,9 +6,9 @@ from timeit import timeit
 from typing import Any, Tuple
 
 if sys.version_info > (3, 9):
-    from collections.abc import Dict
+    from collections.abc import Mapping
 else:
-    from typing import Dict
+    from typing import Mapping
 
 from memory_profiler import memory_usage, profile
 
@@ -26,7 +26,7 @@ class BenchmarkResult:
     def memory(self) -> float:
         return self._memory
 
-    def dict(self) -> Dict[str, float]:
+    def dict(self) -> Mapping[str, float]:
         return {"timing": self.timing, "memory": self.memory}
 
 
@@ -78,3 +78,65 @@ class BenchmarkBase(metaclass=ABCMeta):
             statistics.mean(timing_buffer),
             max(m2 - m1 for m1, m2 in zip(memory_after_setup_buffer, memory_during_exec_buffer)),
         )
+
+
+class BenchmarkRatio:
+    def __init__(self, timing: float, memory: float):
+        self._timing = timing
+        self._memory = memory
+
+    @property
+    def timing(self) -> float:
+        return self._timing
+
+    @property
+    def memory(self) -> float:
+        return self._memory
+
+    def dict(self) -> Mapping[str, float]:
+        return {"timing": self.timing, "memory": self.memory}
+
+    @classmethod
+    def calc(self, one: BenchmarkResult, another: BenchmarkResult) -> "BenchmarkRatio":
+        if one.timing == 0.0:
+            if another.timing == 0.0:
+                timing_ratio = 1.0
+            else:
+                timing_ratio = float("inf")
+        else:
+            timing_ratio = another.timing / one.timing
+        if one.memory == 0.0:
+            if another.memory == 0.0:
+                memory_ratio = 1.0
+            else:
+                memory_ratio = float("inf")
+        else:
+            memory_ratio = another.memory / one.memory
+        return BenchmarkRatio(timing_ratio, memory_ratio)
+
+
+class ComparisonResult:
+    def __init__(self, one: BenchmarkResult, another: BenchmarkResult):
+        self._one = one
+
+        self._another = another
+
+    @property
+    def ratio(self) -> BenchmarkRatio:
+        return BenchmarkRatio.calc(self._one, self._another)
+
+    def dict(self) -> Mapping[str, Mapping[str, float]]:
+        return {"one": self._one.dict(), "another": self._another.dict(), "ratio": self.ratio.dict()}
+
+
+class Comparison:
+    def __init__(self, one: BenchmarkBase, another: BenchmarkBase):
+        self._one = one
+        self._another = another
+
+    def __call__(
+        self,
+    ) -> ComparisonResult:
+        one_result = self._one()
+        another_result = self._another()
+        return ComparisonResult(one_result, another_result)
