519e71f9f586ac45256da93da0e10a76deb000c8
osoken <osoken.devel@outlook.jp>
2021-11-14 23:10:59 +0900

    feature(docs/benchmarks/list.py): add some benchmarks on list methods

---
+++ docs/benchmarks/list.py
@@ -0,0 +1,94 @@
+import gc
+import os
+import random
+
+from sqlitecollections import List
+
+from common import BenchmarkBase, Comparison
+
+benchmarks_dir = os.path.dirname(os.path.abspath(__file__))
+
+target_list = list(range(10000))
+random.seed(5432)
+random.shuffle(target_list)
+
+
+class BuiltinListBenchmarkBase(BenchmarkBase):
+    def __init__(self):
+        super(BuiltinListBenchmarkBase, self).__init__()
+        self._sut_orig = target_list.copy()
+
+    def setup(self):
+        self._sut = self._sut_orig.copy()
+
+    def teardown(self):
+        del self._sut
+        gc.collect()
+        gc.collect()
+
+
+class SqliteCollectionsListBenchmarkBase(BenchmarkBase):
+    def __init__(self):
+        super(SqliteCollectionsListBenchmarkBase, self).__init__()
+        self._sut_orig = List[int](data=target_list)
+
+    def setup(self):
+        self._sut = self._sut_orig.copy()
+
+    def teardown(self):
+        del self._sut
+
+
+class BenchmarkDelitemBase(BenchmarkBase):
+    def exec(self):
+        del self._sut[5000]
+        return self._sut
+
+    def assertion(self, result):
+        return len(result) == 9999 and result[4999] == target_list[4999] and result[5000] == target_list[5001]
+
+
+class BenchmarkGetitemBase(BenchmarkBase):
+    def exec(self):
+        return self._sut[5000]
+
+    def assertion(self, result):
+        return result == target_list[5000]
+
+
+class BenchmarkGetitemSliceBase(BenchmarkBase):
+    def exec(self):
+        return self._sut[10:5010]
+
+    def assertion(self, result):
+        return all([a == b for a, b in zip(target_list[10:5010], result)])
+
+
+class BuiltinListBenchmarkDelitem(BuiltinListBenchmarkBase, BenchmarkDelitemBase):
+    pass
+
+
+class SqliteCollectionsListBenchmarkDelitem(SqliteCollectionsListBenchmarkBase, BenchmarkDelitemBase):
+    pass
+
+
+class BuiltinListBenchmarkGetitem(BuiltinListBenchmarkBase, BenchmarkGetitemBase):
+    pass
+
+
+class SqliteCollectionsListBenchmarkGetitem(SqliteCollectionsListBenchmarkBase, BenchmarkGetitemBase):
+    pass
+
+
+class BuiltinListBenchmarkGetitemSlice(BuiltinListBenchmarkBase, BenchmarkGetitemSliceBase):
+    pass
+
+
+class SqliteCollectionsListBenchmarkGetitemSlice(SqliteCollectionsListBenchmarkBase, BenchmarkGetitemSliceBase):
+    pass
+
+
+if __name__ == "__main__":
+    print(Comparison(BuiltinListBenchmarkDelitem(), SqliteCollectionsListBenchmarkDelitem())().dict())
+    print(Comparison(BuiltinListBenchmarkGetitem(), SqliteCollectionsListBenchmarkGetitem())().dict())
+    print(Comparison(BuiltinListBenchmarkGetitemSlice(), SqliteCollectionsListBenchmarkGetitemSlice())().dict())
