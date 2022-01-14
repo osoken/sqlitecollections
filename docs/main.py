59ad405802b2058b12fd254dba7b663241d58255
osoken <osoken.devel@outlook.jp>
2022-01-14 23:36:15 +0900

    docs(benchmark): add benchmark value

--- docs/main.py
+++ docs/main.py
@@ -1,5 +1,18 @@
+import os
+
 import sqlitecollections as sc
 
+wd = os.path.dirname(os.path.abspath(__file__))
 
 def define_env(env):
     env.variables["package_version"] = sc.__version__
+
+    env.variables["benchmarks"] = "\n".join(f"""
+=== "{p}"
+    === "dict"
+        {{!benchmark_results/{p}/dict.md!}}
+    === "list"
+        {{!benchmark_results/{p}/list.md!}}
+    === "set"
+        {{!benchmark_results/{p}/set.md!}}
+""" for p in os.listdir(os.path.join(wd, "benchmark_results")) if os.path.isdir(os.path.join(wd, "benchmark_results", p)))
