63da10732e59c40db4a9b16a7e20b0717d14b786
osoken <osoken.devel@outlook.jp>
2022-01-14 23:49:03 +0900

    docs(benchmark): tidy result order

--- docs/main.py
+++ docs/main.py
@@ -1,13 +1,16 @@
 import os
+import re
 
 import sqlitecollections as sc
 
 wd = os.path.dirname(os.path.abspath(__file__))
 
+
 def define_env(env):
     env.variables["package_version"] = sc.__version__
 
-    env.variables["benchmarks"] = "\n".join(f"""
+    env.variables["benchmarks"] = "\n".join(
+        f"""
 === "{p}"
     === "dict"
         {{!benchmark_results/{p}/dict.md!}}
@@ -15,4 +18,9 @@ def define_env(env):
         {{!benchmark_results/{p}/list.md!}}
     === "set"
         {{!benchmark_results/{p}/set.md!}}
-""" for p in os.listdir(os.path.join(wd, "benchmark_results")) if os.path.isdir(os.path.join(wd, "benchmark_results", p)))
+"""
+        for p in sorted(
+            (os.listdir(os.path.join(wd, "benchmark_results"))), key=lambda x: int(re.sub(r"[^0-9]", "", x) + "0")
+        )
+        if os.path.isdir(os.path.join(wd, "benchmark_results", p))
+    )
