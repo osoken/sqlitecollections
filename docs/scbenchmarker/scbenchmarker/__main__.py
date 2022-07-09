a73d25be79d1eb29971ef0719178d3b1cddd6177
osoken <osoken.devel@outlook.jp>
2022-07-09 21:36:38 +0900

    fix(docs): add result-cache argument

--- docs/scbenchmarker/scbenchmarker/__main__.py
+++ docs/scbenchmarker/scbenchmarker/__main__.py
@@ -39,35 +39,38 @@ def parse_target(s: str) -> Tuple[str]:
 
 if __name__ == "__main__":
     wd = os.path.dirname(os.path.abspath(__file__))
+
     parser = ArgumentParser()
     parser.add_argument("--prefix", default="benchmarks")
+    parser.add_argument("--result-cache")
+
     subcommand_parser = parser.add_subparsers(dest="subcommand")
+
     benchmarking_parser = subcommand_parser.add_parser("benchmarking")
     benchmarking_parser.add_argument("--timeout", default=None, type=float)
     benchmarking_parser.add_argument("--debug", action="store_true")
-    benchmarking_parser.add_argument("--output-path")
+
     benchmarking_parser.add_argument("targets", nargs="*")
+
     render_parser = subcommand_parser.add_parser("render")
-    render_parser.add_argument("--input-path")
+
     args = parser.parse_args()
+
+    cache_path = args.result_cache or os.path.join(
+        os.path.dirname(os.path.dirname(wd)), "temp", f"benchmark_{args.prefix}.db"
+    )
+    cache_dir = os.path.dirname(cache_path)
+
+    cache_dict = sc.Dict[str, dict](
+        connection=cache_path,
+        table_name="benchmark_results",
+        key_serializer=lambda x: x.encode("utf-8"),
+        key_deserializer=lambda x: x.decode("utf-8"),
+        value_serializer=lambda x: json.dumps(x).encode("utf-8"),
+        value_deserializer=lambda x: json.loads(x.decode("utf-8")),
+    )
+
     if args.subcommand == "benchmarking":
-        output_path = args.output_path or os.path.join(
-            os.path.dirname(os.path.dirname(wd)), "temp", f"benchmark_{args.prefix}.db"
-        )
-        output_dir = os.path.dirname(output_path)
-
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
-
-        buf = sc.Dict[str, dict](
-            connection=output_path,
-            table_name="benchmark_results",
-            key_serializer=lambda x: x.encode("utf-8"),
-            key_deserializer=lambda x: x.decode("utf-8"),
-            value_serializer=lambda x: json.dumps(x).encode("utf-8"),
-            value_deserializer=lambda x: json.loads(x.decode("utf-8")),
-        )
-
         for fn in filter(lambda x: x.startswith("benchmark_") and x.endswith(".py"), os.listdir(wd)):
             container_type_str = re.sub(r"benchmark_([a-z]+)\.py", "\\1", fn)
             m = importlib.import_module("scbenchmarker.{}".format(re.sub('\\.py$', '', fn)))
@@ -106,7 +109,7 @@ if __name__ == "__main__":
                     builtin_benchmark_class(timeout=args.timeout, debug=args.debug),
                     sqlitecollections_benchmark_class(timeout=args.timeout, debug=args.debug),
                 )
-                buf[f"{fn}::{comp._subject}"] = comp().dict()
+                cache_dict[f"{fn}::{comp._subject}"] = comp().dict()
                 print(".", end="")
             print("")
     elif args.subcommand == "render":
